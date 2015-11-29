24c657b878c41efc41403955de54f590a75cf963
Taylor Monacelli <taylor.monacelli@streambox.com>
2015-11-29 14:31:15 -0800

    Broken when trying to use 2 instances of same module in 2 regions
    Error applying plan:
    
    5 error(s) occurred:
    
    * aws_subnet.private.0: Error creating subnet: InvalidParameterValue: Value (us-west-2a) for parameter availabilityZone is invalid. Subnets can currently only be created in the following availability zones: us-west-1b, us-west-1c.
    	status code: 400, request id:
    * aws_subnet.private.1: Error creating subnet: InvalidParameterValue: Value (us-west-2b) for parameter availabilityZone is invalid. Subnets can currently only be created in the following availability zones: us-west-1b, us-west-1c.
    	status code: 400, request id:
    * aws_subnet.public.1: Error creating subnet: InvalidParameterValue: Value (us-west-2b) for parameter availabilityZone is invalid. Subnets can currently only be created in the following availability zones: us-west-1b, us-west-1c.
    	status code: 400, request id:
    * aws_subnet.public.0: Error creating subnet: InvalidParameterValue: Value (us-west-2a) for parameter availabilityZone is invalid. Subnets can currently only be created in the following availability zones: us-west-1b, us-west-1c.
    	status code: 400, request id:
    * aws_vpc_endpoint.s3: Error creating VPC Endpoint: InvalidServiceName: Invalid Service Name: 'com.amazonaws.us-west-2.s3' (expecting 'com.amazonaws.<region>.<service_name>')
    	status code: 400, request id:
    
    Terraform does not automatically rollback in the face of errors.
    Instead, your Terraform state file has been partially updated with
    any resources that successfully completed. Please address the error
    above and apply again to incrementally change your infrastructure.
    [demo@demos-MBP:~/pdev/terraform-aws-vpc-test(master)]$

--- main.tf
+++ main.tf
@@ -1,5 +1,10 @@
 provider "aws" {
-  region = "${var.region}"
+  region = "us-west-1"
+}
+
+provider "aws" {
+  region = "us-west-2"
+  alias = "west2"
 }
 
 module "vpc" {
@@ -16,5 +21,24 @@ module "vpc" {
   nat_ami = "ami-54ea8634"
   nat_instance_type = "t2.micro"
   bastion_ami = "ami-54ea8634"
+  bastion_instance_type = "t2.micro"
+}
+
+module "vpc-us-west-2" {
+  provider = "aws.west2"
+
+  source = "git::https://github.com/TaylorMonacelli/terraform-aws-vpc.git?ref=wip"
+
+  name = "Default"
+  region = "us-west-2"
+  key_name = "taylor-terraform"
+  cidr_block = "10.0.0.0/16"
+  external_access_cidr_block = "0.0.0.0/0"
+  private_subnet_cidr_blocks = "10.0.1.0/24,10.0.3.0/24"
+  public_subnet_cidr_blocks = "10.0.0.0/24,10.0.2.0/24"
+  availability_zones = "us-west-2a,us-west-2b,us-west-2c"
+  nat_ami = "ami-d93622b8"
+  nat_instance_type = "t2.micro"
+  bastion_ami = "ami-d93622b8"
   bastion_instance_type = "t2.micro"
 }
